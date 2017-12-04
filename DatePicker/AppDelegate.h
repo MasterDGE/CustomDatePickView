4c964e52801c9a5e4801c5ca5e40370e1cb11bfe
Zax <worklife_dj@outlook.com>
2017-12-04 15:35:26 +0800

    Initial Commit

---
+++ DatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatePicker
+//
+//  Created by Zax on 2017/12/4.
+//  Copyright © 2017年 Shirley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
